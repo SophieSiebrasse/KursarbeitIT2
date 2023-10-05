315a6ae8dc7412326e7750c6eb13471ac5cd9612
SophieSiebrasse <126667692+SophieSiebrasse@users.noreply.github.com>
2023-10-05 11:14:04 +0200

    update

--- ssiebrasse/index.php
+++ ssiebrasse/index.php
@@ -6,8 +6,8 @@
 </head>
 
 <body>
-	<a href="Fallzeit/Fallzeit/Fallzeit.php">Aufgabe 1 "Fallzeit"</a>
-	<a href="Fallzeit/Tank/tank.php">Aufgabe 2 "Tank"</a>
-	<a href="Fallzeit/Adresse/index.php">Aufgabe Vorlesung 05.10 "Adresse"</a>
+	<a href="Fallzeit/Fallzeit.php">Aufgabe 1 "Fallzeit"</a>
+	<a href="Tank/tank.php">Aufgabe 2 "Tank"</a>
+	<a href="/Adresse/adresse.php">Aufgabe Vorlesung 05.10 "Adresse"</a>
 </body>
 </html>
