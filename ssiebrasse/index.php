8aaac1a0184a573e4aaef038336292c71088809e
SophieSiebrasse <126667692+SophieSiebrasse@users.noreply.github.com>
2023-10-05 11:29:21 +0200

    update

--- ssiebrasse/index.php
+++ ssiebrasse/index.php
@@ -8,7 +8,7 @@
 <body>
 	<a href="Fallzeit/Fallzeit.php">Aufgabe 1 "Fallzeit"</a>
 	<a href="Tank/tank.php">Aufgabe 2 "Tank"</a>
-	<a href="Adresse 05.10/Übung.php" >"Uebung"</a>
+	<a href="Adresse 05.10/Adresse.php">"Adresse"</a>
 	
 </body>
 </html>
