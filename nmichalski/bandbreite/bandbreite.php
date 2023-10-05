05b58ed4d7282a6a19d69c6660f3fec1d88615b3
Nina Michalski <126667852+ninamichalski7@users.noreply.github.com>
2023-10-05 08:54:31 +0200

    update

--- nmichalski/bandbreite/bandbreite.php
+++ nmichalski/bandbreite/bandbreite.php
@@ -11,11 +11,12 @@ echo "<h1>Aufgabe 6 vom 28. September</h1>";
 echo "<p>Schreiben Sie ein Programm, welches die Zeit berechnet, die eine Dateiübertragung in Anspruch nimmt, wenn die Dateigröße sowie die Bandbreite gegeben ist. Entscheiden Sie eigenständig, in welcher Einheit die Werte angegeben werden müssen.</p>";
 
 //Zuweisung der Variablen
-$dateigroesse=20;
-$bandbreite=200;
+$dateigroesseByte=70;
+$bandbreiteBit=2000;
 	
-$zeit=bandbreite/dateigroesse;
+$zeit=bandbreiteBit*dateigroesseByte;
 echo($zeit);
+	
 ?>
 	
 	
