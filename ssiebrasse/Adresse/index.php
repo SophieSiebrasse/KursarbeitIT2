9451103432d060222743567aa026488922908459
SophieSiebrasse <126667692+SophieSiebrasse@users.noreply.github.com>
2023-10-05 09:18:27 +0200

    Update

---
+++ ssiebrasse/Adresse/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Seite von Sophie Siebrasse</title>
+</head>
+
+<body>
+	<a href="Adresse/Adresse.php">Aufgabe Vorlesung 05.10 "Adresse"</a>
+	
+</body>
+</html>
