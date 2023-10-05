76bf77cadcacead5fd215a47b14a3e0882288894
SophieSiebrasse <126667692+SophieSiebrasse@users.noreply.github.com>
2023-10-05 10:06:43 +0200

    update

--- ssiebrasse/Adresse/index.php
+++ ssiebrasse/Adresse/index.php
@@ -9,10 +9,20 @@
 	<a href="Adresse/Adresse.php">Aufgabe Vorlesung 05.10 "Adresse"</a>
 	
 	<form action="adresse.php" method="post" >
-		</p><input name="vor"> Vorname</p>
+		</p> Vorname</p>
 	    </p><input name="nach"> Nachname</p>
         </p><input type= "submit"></p>
-    </form>
+</form>
+<?php
+$a = 435;
+echo intval($a)."<br>";
+
+$b="22.6";
+echo intval ($b). "<br>";
+echo doubleval($b). "<br>";
+
+?>
+	
 	
 </body>
 </html>
