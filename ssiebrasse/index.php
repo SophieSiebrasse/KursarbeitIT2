2a442556a8aa57ae0ac9e5a931b68e47e0963ab5
SophieSiebrasse <126667692+SophieSiebrasse@users.noreply.github.com>
2023-10-08 19:30:24 +0200

    update

--- ssiebrasse/index.php
+++ ssiebrasse/index.php
@@ -20,27 +20,6 @@
 <title>Formular</title>
 </head>
 
-<body>
-	<h1>Das erste Programm</h1>
-	<form action="../adresse.php" method="post">
-		<p><input name="vor">Vorname</p>
-		<p><input name="nach">Nachname</p>
-		<p><input type="submit"></p>
-	</form>
-	<?php
-		$a = 435;
-		echo intval($a)."<br>";
-	
-	$b="22.6";
-	echo intval($b)."<br>";
-	echo doubleval($b)."<br>";
-	
-	?>
-	
-	
-	</form>
-</body>
-</html>
 
 <!doctype html>
 <html>
