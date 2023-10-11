44e6bf08c3116e7cd617b4b8b3e293a752f76c9d
Nina Michalski <126667852+ninamichalski7@users.noreply.github.com>
2023-10-11 23:27:24 +0200

    update

--- Vorlesungswoche 3/index.php
+++ Vorlesungswoche 3/index.php
@@ -4,9 +4,19 @@
 <meta charset="UTF-8">
 <title>Vorlesungswoche 3</title>
 </head>
-	<h1> Vorlesungswoche 3</h1>
+	<h1> Aufgaben Vorlesungswoche 3</h1>
 
-<a href="Aufgabe 1/fallzeit.php" > Aufgabe 1 </a>
+	<a href="Aufgabe 1/fallzeit.php" > Aufgabe 1 </a>;
+	
+	<a href="Aufgabe 2/tankmenge.php" > Aufgabe 2 </a>;
+	
+	<a href="Aufgabe 3/funktion.php" > Aufgabe 3</a>;
+	
+	<a href="Aufgabe 4/seite.php" > Aufgabe 4 </a>;
+	
+	<a href="Aufgabe 5/bmi.php" > Aufgabe 5</a>;
+	
+	<a href="Aufgabe 6/dateigroesse.php" > Aufgabe 6 </a>;
 <body>
 </body>
 </html>
