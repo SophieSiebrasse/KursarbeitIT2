f78801dbbc7895b39c6502618716856b71ebae13
Nina Michalski <126667852+ninamichalski7@users.noreply.github.com>
2023-10-12 00:09:08 +0200

    Update

--- Vorlesungswoche 3/Aufgabe 4/seite.php
+++ Vorlesungswoche 3/Aufgabe 4/seite.php
@@ -2,8 +2,30 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Unbenanntes Dokument</title>
+<title>Aufgabe 4</title>
 </head>
+	
+	<h1>Aufgabe 4</h1>
+	<p>Seite a und die Seite b dürfen nicht 0 sein.</p>
+	
+<?php
+
+function pruefeSeiten($a, $b) {
+    if ($a != 0 && $b != 0) {
+        echo "Seite a und Seite b sind beide nicht 0.";
+    } else {
+        echo "Fehler: Seite a oder Seite b ist 0.";
+    }
+}
+
+// Beispielaufruf
+$seite_a = intval(readline("Gib den Wert für Seite a ein: "));
+$seite_b = intval(readline("Gib den Wert für Seite b ein: "));
+
+pruefeSeiten($seite_a, $seite_b);
+
+?>
+
 
 <body>
 </body>
--- Vorlesungswoche 3/Aufgabe 6/dateigroesse.php
+++ Vorlesungswoche 3/Aufgabe 6/dateigroesse.php
@@ -2,8 +2,29 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Unbenanntes Dokument</title>
+<title>Aufgabe 6</title>
 </head>
+	<h1>Aufgabe 6</h1>
+	<p>Die Dateigröße darf nicht 0 sein. Die Bandbreite darf nicht 0 sein.</p>
+
+	<?php
+
+function pruefeDateiUndBandbreite($dateiGroesse, $bandbreite) {
+    if ($dateiGroesse != 0 && $bandbreite != 0) {
+        echo "Die Dateigröße und die Bandbreite sind beide nicht 0.";
+    } else {
+        echo "Fehler: Die Dateigröße oder die Bandbreite ist 0.";
+    }
+}
+
+// Beispielaufruf
+$dateiGroesse = intval(readline("Gib die Dateigröße ein (in Bytes): "));
+$bandbreite = intval(readline("Gib die Bandbreite ein (in bps): "));
+
+pruefeDateiUndBandbreite($dateiGroesse, $bandbreite);
+
+?>
+
 
 <body>
 </body>
