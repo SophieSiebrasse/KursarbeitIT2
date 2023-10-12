e57376c6e607a2a601ddd1cb0b7a1208ca5f2544
Nina Michalski <126667852+ninamichalski7@users.noreply.github.com>
2023-10-12 11:29:51 +0200

    update

--- 4. Vorlesungswoche/schleifen.php
+++ 4. Vorlesungswoche/schleifen.php
@@ -8,17 +8,23 @@
 <body>
 	<h1>Schleifen</h1>
 	
-	
-	
+	<table border=1">
 	<?php
-		for($schleifenzaehler=1;
-		   $schleifenzaehler<=100;
-		   $schleifenzaehler=$schleifenzaehler*4)
+		for($zeilenzaehler=1; $zeilenzaehler<=10; $zeilenzaehler++)
 		{
-			echo "Zeile 
-			$schleifenzaehler<br>";
+			echo "<tr>";
+			for($spaltenzaehler=1; $spaltenzaehler<=10;
+			   $spaltenzaehler++)
+			{
+				$ergebnis=$zeilenzaehler*$spaltenzaehler;
+				echo "<td>";
+				echo $ergebnis;
+				echo "</td>";
+			}
+			echo "</tr>";
 		}
 	?>
-	
+	</table>
+					
 </body>
 </html>
