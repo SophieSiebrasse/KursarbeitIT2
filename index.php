7124cdc5dab96e2d8bb74fc7a28c1a337180efa5
Nina Michalski <126667852+ninamichalski7@users.noreply.github.com>
2023-10-13 23:04:55 +0200

    update

--- index.php
+++ index.php
@@ -6,11 +6,12 @@
 </head>
 
 <body>
-	<a href="nmichalski/" > Hier gehts zu den Seiten von NMichalski</a>
-	<a href="jhindemith/" > Hier gehts zu den Seiten von JHindemith</a>
-	<a href="ssiebrasse/" > Hier gehts zu den Seiten von SSiebrasse</a>
-	<a href="3. Vorlesungswoche /index.php" > Vorlesungswoche 3 </a>
-	<a href="4. Vorlesungswoche/schleifen.php" > Vorlesungswoche 4</a>
+	<a href="nmichalski/" > Hier gehts zu den Seiten von NMichalski</a> <br>
+	<a href="jhindemith/" > Hier gehts zu den Seiten von JHindemith</a> <br>
+	<a href="ssiebrasse/" > Hier gehts zu den Seiten von SSiebrasse</a> <br>
+	<a href="3. Vorlesungswoche /index.php" > Vorlesungswoche 3 </a> <br>
+	<a href="4. Vorlesungswoche/schleifen.php" >Schleifen Übung</a> <br>
+	<a href="4. Vorlesungswoche/index.php" >Vorlesungswoche 4</a> <br>
 	
 	
 </body>
