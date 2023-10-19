c1deb151bb2d1cf37cb2ae468e7740de48f4bf04
Nina Michalski <126667852+ninamichalski7@users.noreply.github.com>
2023-10-19 08:58:23 +0200

    Update

--- public_html/index.php
+++ public_html/index.php
@@ -4,7 +4,12 @@
 <meta charset="UTF-8">
 <title>Teamwebsite</title>
 </head>
-
+ 
 <body>
+	<a href="pages/head.php" >Head</a> <br>
+	<a href="pages/menue.php" >Menü</a> <br>
+	<a href="pages/content.php" >Content</a> <br>
+	<a href="pages/footer.php" >Footer</a> <br>
+	
 </body>
 </html>
