a1a90434f01bf12f00ff0e46ec3da3fd6386e116
Nina Michalski <126667852+ninamichalski7@users.noreply.github.com>
2023-10-26 11:09:35 +0200

    update

--- uebungsaufgabe 26.10./wuerfel.php
+++ uebungsaufgabe 26.10./wuerfel.php
@@ -4,11 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Würfel Berechnung</title>
-	
+    <title>Würfel Berechnung mit Wertetabelle</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 50%;
+            margin-top: 20px;
+        }
+
+        table, th, td {
+            border: 1px solid black;
+        }
+
+        th, td {
+            padding: 10px;
+            text-align: left;
+        }
+    </style>
 </head>
 <body>
-	<h1>Würfelberechnung</h1>
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Werte aus dem Formular erhalten
@@ -25,9 +39,15 @@
             echo "<p>Volumen: $volumen</p>";
             echo "<p>Oberfläche: $oberflaeche</p>";
             echo "<p>Mantelfläche: $mantelflaeche</p>";
+
+            // Wertetabelle erstellen
+            echo "<table>";
+            echo "<tr><th>Kantenlänge</th><th>Volumen</th><th>Oberfläche</th><th>Mantelfläche</th></tr>";
+            echo "<tr><td>$kantenlaenge</td><td>$volumen</td><td>$oberflaeche</td><td>$mantelflaeche</td></tr>";
+            echo "</table>";
         } else {
             // Fehlermeldung ausgeben
-            echo "<p>Die Kantenlänge muss größer als 0 sein.</p>";
+            echo "<p>Die eingegebene Kantenlänge muss größer als 0 sein.</p>";
         }
     }
     ?>
@@ -37,23 +57,5 @@
         Kantenlänge: <input type="text" name="kantenlaenge"><br>
         <input type="submit" value="Berechnen">
     </form>
-	
-	<style>
-        table {
-            border-collapse: collapse;
-            width: 50%;
-            margin-top: 20px;
-        }
-
-        table, th, td {
-            border: 1px solid black;
-        }
-
-        th, td {
-            padding: 10px;
-            text-align: left;
-        }
-    </style>
-	
 </body>
 </html>
