d03a4b168207cf3990eefc3657a6211bbdbc9167
Jule Hindemith <126667913+ju2899@users.noreply.github.com>
2023-11-13 11:11:57 +0100

    update

---
+++ jhindemith/dbconnect.inc.php
@@ -0,0 +1,4 @@
+<?php
+	$con= mysqli_connect("m12241-10.kurs.jade-hs.de","m12241-10","9PXWaAFA7","m12241-10_2");
+	// mysqli_connect("DB-Server","Benutzername","Kennwort","Datenbank");
+?>
---
+++ jhindemith/include.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Unbenanntes Dokument</title>
+</head>
+
+<body>
+	<h1>Umfrage</h1>
+	<?php include 'dbconnect.inc.php' ; ?>
+</body>
+</html>
