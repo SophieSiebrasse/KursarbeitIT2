bff707a0eed165dcc5d9430701713c8cdd862c81
Nina Michalski <126667852+ninamichalski7@users.noreply.github.com>
2023-11-16 10:20:05 +0100

    update

---
+++ nmichalski/mysql.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Umfrage-Tool</title>
+</head>
+
+<body>
+	<!---
+		1. Formular erstellen
+		2. Was passiert beim Abschicken des Formulars?
+			-> Seite ruft sich selber auf
+				->Wann wird der PHP Code für das eintragen in die Datenbank ausgeführt?
+			
+	---!>
+</body>
+</html>
